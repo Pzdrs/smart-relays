0d18437fcfbbd2801b5bde3351712decbb0e384c
Petr (Desktop) <petrbohac3@seznam.cz>
2023-05-04 17:39:13 +0200

    test channel
    Took 5 minutes

--- relays/utils/gpio.py
+++ relays/utils/gpio.py
@@ -1,5 +1,15 @@
+import time
+
 from relays.models import Channel
+import RPi.GPIO as GPIO
 
 
 def test_channel(channel: Channel):
-    pass
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(channel.pin, GPIO.OUT)
+    for _ in range(3):
+        GPIO.output(channel.pin, GPIO.LOW)
+        time.sleep(0.5)
+
+        GPIO.output(channel.pin, GPIO.HIGH)
+        time.sleep(0.5)
