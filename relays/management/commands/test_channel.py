3c1424046abfb80ca67d2e7edb574c91a4e2ef43
Petr (Desktop) <petrbohac3@seznam.cz>
2023-05-04 22:44:55 +0200

    test command params change
    Took 2 minutes

--- relays/management/commands/test_channel.py
+++ relays/management/commands/test_channel.py
@@ -1,9 +1,8 @@
 from django.core.management.base import BaseCommand
 
-from relays.models import Channel
 from relays.tasks import test_channel
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        test_channel(Channel.objects.get(pk=1))
+        test_channel(1)
