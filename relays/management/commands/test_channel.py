8dc841577522755dd453393b2a40f5b10e89febd
Petr (Desktop) <petrbohac3@seznam.cz>
2023-05-04 17:59:39 +0200

    channel test command
    Took 6 minutes

---
+++ relays/management/commands/test_channel.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+
+from relays.models import Channel
+from relays.utils.gpio import test_channel
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        test_channel(Channel.objects.get(pk=1))
