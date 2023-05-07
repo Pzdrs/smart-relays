2e01c7b3af26dff7e824f09a3426c547c773eda4
Petr (Desktop) <petrbohac3@seznam.cz>
2023-05-07 11:57:42 +0200

    tohle je mnohem vic cleaner
    Took 1 minute

--- relays/middleware.py
+++ relays/middleware.py
@@ -11,10 +11,10 @@ class SetupWizardMiddleware(MiddlewareMixin):
         super().__init__(get_response)
 
     def __call__(self, request: HttpRequest):
-        try:
-            setup_wizard_data = ApplicationData.objects.get(key='setup_wizard')
-        except ApplicationData.DoesNotExist:
-            setup_wizard_data = ApplicationData.objects.create(key='setup_wizard', data={'completed': False, 'step': 0})
+        setup_wizard_data = ApplicationData.objects.get_or_create(
+            key='setup_wizard',
+            data={'completed': False, 'step': 0}
+        )
         if not setup_wizard_data.data['completed']:
             if request.path != reverse('relays:setup-wizard'):
                 return redirect(reverse('relays:setup-wizard'))
