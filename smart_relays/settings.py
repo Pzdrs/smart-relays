684276a42ff1fb5009fbd49771944be9d8dfb663
Petr (Laptop) <petrbohac3@seznam.cz>
2023-05-16 18:53:53 +0200

    fix progess bar

--- smart_relays/settings.py
+++ smart_relays/settings.py
@@ -29,10 +29,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-
 AUTH_USER_MODEL = 'accounts.User'
 
 # Application definition
